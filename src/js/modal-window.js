082653f66035282f8bdc30162408a34f24f4e67a
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-01-15 15:07:25 +0200

    refactored from main

--- src/js/modal-window.js
+++
@@ -1,6 +0,0 @@
-const modalMenu = document.querySelector('.mobile-menu');
-const menuToggler = document.querySelector('[data-action=open-modal]');
-
-const handleMenuButton = () => modalMenu.classList.toggle('is-open');
-
-menuToggler.addEventListener('click', handleMenuButton);
---
+++ src/js/modal-windows.js
@@ -0,0 +1,25 @@
+(() => {
+  const modals = {
+    address: document.querySelector('[data-modal="modal-address"]'),
+    form: document.querySelector('[data-modal="modal-form"]'),
+    article: document.querySelector('[data-modal="modal-article"]'),
+  };
+
+  const togglers = {
+    openAddress: document.querySelector('[data-action="modal-open-address"]'),
+    closeAddress: document.querySelector('[data-action="modal-close-address"]'),
+    openForm: document.querySelector('[data-action="modal-open-form"'),
+    closeForm: document.querySelector('[data-action="modal-close-form"'),
+    openArticle: document.querySelector('[data-action="modal-open-article"'),
+    closeArticle: document.querySelector('[data-action="modal-close-article"'),
+  };
+
+  const handleModal = modal => modal.classList.toggle('expanded');
+
+  togglers.openAddress.addEventListener('click', () => handleModal(modals.address));
+  togglers.closeAddress.addEventListener('click', () => handleModal(modals.address));
+  togglers.openForm.addEventListener('click', () => handleModal(form.address));
+  togglers.closeForm.addEventListener('click', () => handleModal(form.address));
+  togglers.openArticle.addEventListener('click', () => handleModal(article.address));
+  togglers.closeArticle.addEventListener('click', () => handleModal(article.address));
+})();
