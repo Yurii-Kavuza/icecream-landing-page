9da50156b7c26ec9d842ff9e28e5c7ca6a406697
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-01-20 01:34:54 +0200

    refactor btn up js

--- src/js/to-top-btn.js
+++ src/js/to-top-btn.js
@@ -1,10 +1,8 @@
 const toTop = document.querySelector('.to-top');
 
-window.addEventListener('scroll', () => {
-  console.log(window.pageYOffset);
-  if (window.pageYOffset > 600) {
-    toTop.classList.add('active');
-  } else {
-    toTop.classList.remove('active');
-  }
-});
+const handleButton = () => {
+  if (window.pageYOffset > 600) toTop.classList.add('active');
+  else toTop.classList.remove('active');
+};
+
+window.addEventListener('scroll', handleButton);
